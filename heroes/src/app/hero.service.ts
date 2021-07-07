db907f979752f534657f8960e44590dde170f38d
Kosai-Al-Ghanom <kosai.alghanom@powercoders.org>
2021-07-07 10:43:02 +0200

    update heroes

--- heroes/src/app/hero.service.ts
+++ heroes/src/app/hero.service.ts
@@ -11,6 +11,9 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
@@ -41,5 +44,13 @@ export class HeroService {
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
+
+  
+updateHero(hero: Hero): Observable<any> {
+  return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap(_ => this.log(`updated hero id=${hero.id}`)),
+    catchError(this.handleError<any>('updateHero'))
+  );
+}
 
 }
